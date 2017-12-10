9e94c562440a817a19c216b1311db0800de617f1
baozi <768588658@qq.com>
2017-12-10 18:07:17 +0800

    change zhe init database.

--- walls_init.sql
+++ walls_init.sql
@@ -24,10 +24,10 @@ CREATE TABLE `walls_comment_for_info` (
   `user_id` bigint(20) NOT NULL COMMENT '评论用户id',
   `content` varchar(128) NOT NULL COMMENT '评论内容',
   `create_time` int(11) NOT NULL COMMENT '评论时间',
-  KEY `id` (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`,`info_id`,`user_id`),
+  FULLTEXT KEY `content` (`content`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COMMENT='万事墙信息评论表';
-
-/*Data for the table `walls_comment_for_info` */
 
 /*Table structure for table `walls_info` */
 
@@ -41,10 +41,10 @@ CREATE TABLE `walls_info` (
   `create_time` int(11) unsigned NOT NULL COMMENT '创建时间',
   `update_time` int(11) DEFAULT NULL COMMENT '修改时间',
   `user_id` bigint(20) NOT NULL COMMENT '发布人id',
-  KEY `id` (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`,`scholl_id`,`user_id`),
+  FULLTEXT KEY `content` (`content`,`address`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COMMENT='万事墙信息表';
-
-/*Data for the table `walls_info` */
 
 /*Table structure for table `walls_reply_for_user_comment` */
 
@@ -53,10 +53,11 @@ CREATE TABLE `walls_reply_for_user_comment` (
   `by_reply_user_id` bigint(20) NOT NULL COMMENT '被回复人编号',
   `reply_user_id` bigint(20) NOT NULL COMMENT '回复人编号',
   `content` varchar(128) NOT NULL COMMENT '回复内容',
-  `create_time` int(11) NOT NULL COMMENT '回复时间'
+  `create_time` int(11) NOT NULL COMMENT '回复时间',
+  PRIMARY KEY (`walls_comment_id`),
+  KEY `walls_comment_id` (`walls_comment_id`,`by_reply_user_id`,`reply_user_id`),
+  FULLTEXT KEY `content` (`content`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='万事墙用户回复表';
-
-/*Data for the table `walls_reply_for_user_comment` */
 
 /*Table structure for table `walls_school` */
 
@@ -65,10 +66,9 @@ CREATE TABLE `walls_school` (
   `name` varchar(128) NOT NULL COMMENT '学校名称',
   `create_time` int(11) NOT NULL COMMENT '创建时间',
   `update_time` int(11) DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`,`name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='万事墙学校表';
-
-/*Data for the table `walls_school` */
 
 /*Table structure for table `walls_user` */
 
@@ -80,13 +80,11 @@ CREATE TABLE `walls_user` (
   `create_time` int(11) NOT NULL COMMENT '创建时间',
   `update_time` int(11) DEFAULT NULL COMMENT '修改时间',
   `status` tinyint(3) NOT NULL COMMENT '状态：1.正常 2.废弃',
-  PRIMARY KEY (`id`,`account`,`status`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='万事墙用户表';
-
-/*Data for the table `walls_user` */
-
-insert  into `walls_user`(`id`,`account`,`password`,`salt`,`create_time`,`update_time`,`status`) values (1,'aaaaaa','','',0,NULL,0);
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account` (`account`),
+  KEY `id` (`id`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='万事墙用户表';
 
 /*Table structure for table `walls_user_info` */
 
@@ -97,20 +95,19 @@ CREATE TABLE `walls_user_info` (
   `header` varchar(256) DEFAULT NULL COMMENT '用户头像地址',
   `create_time` int(11) NOT NULL COMMENT '创建时间',
   `update_time` int(11) DEFAULT NULL COMMENT '修改时间',
-  KEY `id` (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`,`user_id`,`nickname`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='万事墙用户信息表';
-
-/*Data for the table `walls_user_info` */
 
 /*Table structure for table `walls_user_school` */
 
 CREATE TABLE `walls_user_school` (
   `school_id` bigint(20) NOT NULL COMMENT '学校id',
   `user_id` bigint(20) NOT NULL COMMENT '用户id',
-  `create_time` int(11) NOT NULL COMMENT '创建时间'
+  `create_time` int(11) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`school_id`),
+  KEY `school_id` (`school_id`,`user_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='万事墙用户学校关系表';
-
-/*Data for the table `walls_user_school` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
