486d092b40cb6a49665a64e9679d461e7358c9a7
baolianzheng <baolianzheng@jiajiashuaxin.com>
2017-12-11 17:21:43 +0800

    regist add redis cache.

---
+++ application/walls/extend/Reids.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\walls\extend;
+
+/**
+ * redis行为控制类
+ * @author baozi
+ * @dateTime 2017-12-11
+ */
+class ClassName extends AnotherClass
+{
+    protected $config = array(
+        'ip' => '127.0.0.1',
+        'port'=>'6379',
+        'password'=>''
+    );
+    function __construct()
+    {
+        
+    }
+
+}
